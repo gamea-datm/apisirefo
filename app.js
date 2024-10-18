f09acdc33c76d04ca182bb92a3f92d5ba99c3402
datm <crv@example.com>
2024-10-18 14:50:48 -0400

    probando sin certificado con nueva url

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 const axios = require('axios');
-const https = require('https');
 
-const fs = require('fs');
-const path = require('path');
-
-const certFile = path.join(__dirname, 'Certificado2024.crt');
-
-const agentWithCert = new https.Agent({
-    keepAlive: true,
-    cert: fs.readFileSync(certFile),
-    rejectUnauthorized: false
-});
 axios({
     method: 'get',
     url: 'https://servicios.asfi.gob.bo/Retencionesdev/ServicioRetencionFondos.svc?wsdl',
-    httpsAgent: agentWithCert,
     timeout: 5000
 })
     .then(response => {
@@ -23,3 +11,4 @@ axios({
     .catch(error => {
         console.error('Error:', error.message);
     });
+// prueba 2
