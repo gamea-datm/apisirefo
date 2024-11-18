9e15fafa21a8bf1ad911b22daf032d7ef8cc30db
datm <crv@example.com>
2024-11-18 16:14:31 -0400

    PRUEBA5

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
 const soap = require('soap');
 
-const url = 'https://srvservicios.asfi.gob.bo/RetencionesDev/ServicioRetencionFondos.svc?wsdl';
+// URL del WSDL
+const wsdlUrl = 'https://srvservicios.asfi.gob.bo/RetencionesDev/ServicioRetencionFondos.svc?wsdl';
 
-const options = {
-  wsdl_headers: {
-    'Content-Type': 'application/soap+xml; charset=utf-8',
-  },
-  wsdl_options: {
-    rejectUnauthorized: false, // Si es necesario omitir errores SSL
-  },
-};
-
-soap.createClient(url, options, function (err, client) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  client.addHttpHeader('Content-Type', 'application/soap+xml; charset=utf-8');
-
-  const args = {
-    Texto: 'Texto de prueba para Ping',
-  };
-
-  client.Ping(args, function (err, result) {
+// Crear el cliente SOAP
+soap.createClient(wsdlUrl, (err, client) => {
     if (err) {
-      console.error(err);
-    } else {
-      console.log(result);
+        console.error('Error al cargar el WSDL:', err);
+        return;
     }
-  });
+
+    console.log('Cliente SOAP creado con éxito.');
+    console.log('Descripción del cliente:', client.describe());
 });
