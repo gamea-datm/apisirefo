629647745ec98621d3f6e4b4beb3c4deaaee6644
datm <crv@example.com>
2024-11-19 10:11:04 -0400

    p13

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 const axios = require('axios');
 const xml2js = require('xml2js');
 
-// Definir la URL del servicio
 const serviceUrl = 'https://srvservicios.asfi.gob.bo/RetencionesDev/ServicioRetencionFondos.svc/Soap';
 
-// Construir manualmente el mensaje SOAP
+// Construcción del mensaje SOAP adaptado al WSDL
 const soapMessage = `
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="https://srvservicios.asfi.gob.bo/RetencionesDev/">
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="https://srvservicios.asfi.gob.bo/RetencionesDev/">
     <soap:Body>
-        <ns:Ping>
-            <ns:InputParameter>PruebaPing</ns:InputParameter>
-        </ns:Ping>
+        <tns:Ping>
+            <tns:InputParameter>PruebaPing</tns:InputParameter>
+        </tns:Ping>
     </soap:Body>
 </soap:Envelope>
 `;
 
-// Realizar la solicitud POST
-axios.post(serviceUrl, soapMessage, {
-    headers: {
-        'Content-Type': 'text/xml; charset=utf-8',
-        'SOAPAction': '"https://srvservicios.asfi.gob.bo/RetencionesDev/IRetencionFondos/Ping"'
-    }
-})
-    .then(async (response) => {
+async function callSoapService() {
+    try {
+        // Realizar la solicitud SOAP
+        const response = await axios.post(serviceUrl, soapMessage, {
+            headers: {
+                'Content-Type': 'text/xml; charset=utf-8',
+                'SOAPAction': '"https://srvservicios.asfi.gob.bo/RetencionesDev/IRetencionFondos/Ping"'
+            }
+        });
+
         console.log("Respuesta completa del servicio:");
         console.log(response.data);
 
@@ -30,13 +31,16 @@ axios.post(serviceUrl, soapMessage, {
         const parser = new xml2js.Parser({ explicitArray: false, tagNameProcessors: [xml2js.processors.stripPrefix] });
         const result = await parser.parseStringPromise(response.data);
 
-        // Extraer datos relevantes de la respuesta
+        // Extraer datos relevantes
         const responseBody = result.Envelope.Body;
         const pingResult = responseBody.PingResponse.PingResult;
 
         console.log("Resultado del servicio:", pingResult);
-    })
-    .catch((error) => {
+    } catch (error) {
         console.error("Error al invocar el servicio:");
         console.error(error.response?.data || error.message);
-    });
+    }
+}
+
+// Llamar al servicio SOAP
+callSoapService();
